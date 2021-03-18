62ed58529c9de5a034af24cfef8c0d39ba531819
denis_goncharov <info@muffs.ru>
2021-03-18 14:02:05 +0300

    improve checks

--- build/app/main.py
+++ build/app/main.py
@@ -56,33 +56,36 @@ def get_image_mime(stream):
         return abort(500)
 
 
-def image_check(image, req):
+def image_check(image_url):
     """Uploaded file checks"""
+    headers = {"Range": "bytes=0-2048"}
+    req = requests.get(image_url, headers=headers, allow_redirects=True, timeout=5)
+    if req.status_code != 206:
+            abort(req.status_code)
     content_length = int(req.headers.get("content-length", None))
     content_type = req.headers.get("content-type")
-    mime = get_image_mime(image)  #get mime type from uploaded file
-    if mime != content_type:
+    image_head = BytesIO(req.content)
+    mime = get_image_mime(image_head)  #get mime type from uploaded file
+    if content_type != mime:
         abort(403, "Content missmatch")
-    if application.config["COOKIE_SIZE_LIMIT"] < content_length:
+    if content_length > application.config["COOKIE_SIZE_LIMIT"] :
         abort(403, "Image is too large")
     return True
 
-
 def image_process(image_url, scale_percent):
-    """image download by url and process"""
-    try:
+#    """image download by url and process"""
+#    try:
         image_url = image_url.decode().rstrip("\n")
-        req = requests.get(image_url, allow_redirects=True, timeout=5)
-        image = BytesIO(req.content)
-        if req.status_code == 200 and image_check(image, req):
-            # check result code and that header and content match
+        if image_check(image_url):
+            req = requests.get(image_url, allow_redirects=True, timeout=5)
+            image = BytesIO(req.content)
             req.raw.decode_content = True
             thumb = make_thumbnail(image, scale_percent)
             return image_to_object(thumb)
-        return abort(req.status_code)
-    except:
-        print("Error in image_process()")
         return abort(500)
+#    except:
+#        print("Error in image_process()")
+#        return abort(500)
 
 
 def handle_scale(scale_percent):
@@ -120,8 +123,8 @@ def make_thumbnail(input_image, scale_size):
 
 @application.route("/", methods=["GET", "PUT"])
 def req_handler():
-    """GET/PUT requests handler"""
-    try:
+#    """GET/PUT requests handler"""
+#    try:
         if request.method == "GET":
             url = request.args.get("url")
             if url:
@@ -137,9 +140,9 @@ def req_handler():
                             image_to_object(
                                 make_thumbnail(file, handle_scale(scale_percent))), mimetype="*/*")
         return redirect("/index.html", code=302)
-    except:
-        print("Error in req_handler()")
-        return abort(500)
+#    except:
+#        print("Error in req_handler()")
+#        return abort(500)
 
 
 @application.errorhandler(405)
